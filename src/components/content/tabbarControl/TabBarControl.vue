6931fdaedf5b94e98eec9062d74608c8acfe547e
JackXu <973157810@qq.com>
2020-04-28 15:02:57 +0800

    首页

---
+++ src/components/content/tabbarControl/TabBarControl.vue
@@ -0,0 +1,63 @@
+<template>
+    <div class="tab-control">
+        <div v-for="(item,index) in titles" class="tab-control-item" :class="{active: index===curentIndex}" @click="tabClick(index)">
+             <span>
+            {{item}}
+             </span>
+        </div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "TabBarControl",
+        data(){
+            return {
+                curentIndex:0
+            }
+        },
+        props: {
+            titles: {
+                type: Array,
+                default() {
+                    return []
+                }
+            }
+        },
+        methods:{
+            tabClick(index){
+                this.curentIndex = index;
+            }
+        }
+    }
+</script>
+
+<style scoped>
+     .tab-control {
+         display: flex;
+         text-align: center;
+         font-size: 15px;
+         background-color: white;
+
+     }
+    .tab-control-item{
+        flex: 1;
+        height: 40px;
+        line-height: 40px;
+    }
+
+     .tab-control-item span{
+         padding: 5px;
+     }
+
+    .active {
+        color: var(--color-high-text);
+
+    }
+
+    .active span {
+        border-bottom:3px solid var(--color-high-text);
+    }
+
+</style>
---
+++ src/views/home/Home.vue
@@ -0,0 +1,100 @@
+<template>
+   <div id="home">
+       <nav-bar class="home-nav"><div slot="center"><p>购物街</p></div></nav-bar>
+       <home-swiper :banners="banners"></home-swiper>
+       <recomment-view :recommends="recommends"></recomment-view>
+       <feature-view></feature-view>
+       <tab-bar-control class="home-tab" :titles="['流行','新款','精选']"></tab-bar-control>
+       <ul>
+           <li>fjdkfjfjd</li>
+           <li>fjdkfjfjd</li>
+           <li>fjdkfjfjd</li>
+           <li>fjdkfjfjd</li>
+           <li>fjdkfjfjd</li>
+           <li>fjdkfjfjd</li>
+           <li>fjdkfjfjd</li>
+           <li>fjdkfjfjd</li>
+           <li>fjdkfjfjd</li>
+           <li>fjdkfjfjd</li>
+           <li>fjdkfjfjd</li>
+           <li>fjdkfjfjd</li>
+           <li>fjdkfjfjd</li>
+           <li>fjdkfjfjd</li>
+           <li>fjdkfjfjd</li>
+           <li>fjdkfjfjd</li>
+           <li>fjdkfjfjd</li>
+           <li>fjdkfjfjd</li>
+           <li>fjdkfjfjd</li>
+           <li>fjdkfjfjd</li>
+           <li>fjdkfjfjd</li>
+           <li>fjdkfjfjd</li>
+           <li>fjdkfjfjd</li>
+           <li>fjdkfjfjd</li>
+           <li>fjdkfjfjd</li>
+           <li>fjdkfjfjd</li>
+           <li>fjdkfjfjd</li>
+           <li>fjdkfjfjd</li>
+           <li>fjdkfjfjd</li> <li>fjdkfjfjd</li>
+           <li>fjdkfjfjd</li>
+           <li>fjdkfjfjd</li>
+           <li>fjdkfjfjd</li>
+
+
+
+       </ul>
+   </div>
+</template>
+
+<script>
+    import NavBar from "components/common/navbar/NavBar";
+    import TabBarControl from "components/content/tabbarControl/TabBarControl";
+
+    import  {getHomeMultidata} from "network/home";
+
+    import HomeSwiper from "./childComps/HomeSwiper";
+    import RecommentView from "./childComps/RecommentView";
+    import FeatureView from "./childComps/FeatureView";
+
+    export default {
+        name: "Home",
+        components:{
+            NavBar,
+            HomeSwiper,
+            RecommentView,
+            FeatureView,
+            TabBarControl
+        },
+
+
+        data(){
+          return {
+              banners: [],
+              recommends: []
+          }
+        },
+        created() {
+            getHomeMultidata().then(res => {
+               this.banners = res.data.banner.list;
+               this.recommends = res.data.recommend.list;
+            }, err => {
+
+            })
+        }
+    }
+</script>
+
+<style scoped>
+    #home {
+        padding-top: 44px;
+    }
+  .home-nav {
+      background-color: var(--color-tint);
+      color: white;
+  }
+
+    .home-tab {
+        position: sticky;
+        top: 44px;
+    }
+
+</style>
